bc9a0bdbcb12d15a533d2bb091166d882a36d785
Vitaly <vitalykri123@gmail.com>
2021-10-03 15:42:36 +0500

    add homework3

--- src/main/java/Lesson2/Homework/OnlineChat/client/SavebleLocalFile.java
+++ src/main/java/Lesson2/Homework/OnlineChat/client/SavebleLocalFile.java
@@ -3,10 +3,12 @@ package Lesson2.Homework.OnlineChat.client;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
 public interface SavebleLocalFile {
 
-    BufferedOutputStream writeToStream();
-    void readingTheStream(BufferedInputStream bis);
+    BufferedWriter writeToStream();
+    void readingTheStream();
 
 }
